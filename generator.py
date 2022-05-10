fixed scaling so tanh works now

generator.py
@@ -28,7 +28,7 @@ class Generator(tf.keras.Model):
             Decoder_Block(256,4),
             Decoder_Block(128,4),
             Decoder_Block(64,4),
-            tf.keras.layers.Conv2DTranspose(2, 4, strides=2, padding='same')
+            tf.keras.layers.Conv2DTranspose(2, 4, strides=2, padding='same', activation='tanh')
         ]
 
         self.optimizer = tf.keras.optimizers.Adam()

preprocessSam.py
@@ -1,23 +1,35 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
-import tensorflow_io as tfio
+from skimage.color import rgb2lab, lab2rgb
 
 # a_channel = (a_channel + 86.185) / 184.439
 # b_channel = (b_channel + 107.863) / 202.345
+def denorm_imgs(images):
+    images = images.numpy()
+    images[...,0] = (images[...,0] + 1) * 50
+    images[...,1:] = images[...,1:] * 128
+    images = lab2rgb(images)
+    return tf.convert_to_tensor(images)
+
+def norm_imgs(images):
+    lab = rgb2lab(images.numpy())
+    lab[...,0] = lab[...,0] / 50. - 1
+    lab[...,1:] = lab[...,1:] / 128
+    return tf.convert_to_tensor(lab)
 
 
-def prepare_image(file, rgb=False):
-    image = tf.cast(file["image"], tf.float32)
+def prepare_image(file):
+    image = tf.cast(file["input"], tf.float32)
 
     normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255.)
 
     image = tf.image.resize_with_crop_or_pad(image, 256, 256)
     image = normalization_layer(image)
 
-    if rgb:
-        image_bw = tf.image.rgb_to_grayscale(image)
-    else:
-        image = tfio.experimental.color.rgb_to_lab(image)
+    # if rgb:
+    #     image_bw = tf.image.rgb_to_grayscale(image)
+    # else:
+    #     image = tfio.experimental.color.rgb_to_lab(image)
     
     
 
@@ -36,8 +48,8 @@ def prepare_image(file, rgb=False):
         
         # print(image_bw.shape)
         # exit(0)
-    image_bw = image[:,:,:1]
-    return image_bw, image
+    # image_bw = image[:,:,:1]
+    return image
 
      
 
@@ -45,7 +57,7 @@ def get_data(batch_size):
     # exit(0)
     # ds = tfds.load('imagenet_v2', split='test', shuffle_files=True)
     # mnist_builder = tfds.builder("stanford_dogs")
-    mnist_builder = tfds.builder("horses_or_humans")
+    mnist_builder = tfds.builder("bee_dataset")
     mnist_builder.download_and_prepare()
     # print("here")
     dataset = mnist_builder.as_dataset()["train"]

train.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import os
 import tensorflow_io as tfio
+from preprocessSam import norm_imgs, denorm_imgs
+# from skimage.color import rgb2lab, lab2rgb
 
 
 checkpoint_directory = "/tmp/training_checkpoints"
@@ -15,9 +17,11 @@ def train_epoch(generator, discriminator, dataset, new=True):
 
     progbar = tf.keras.utils.Progbar(len(list(dataset)))
 
-    for i, (bw_images, color_images) in enumerate(dataset):
+    for i, color_images in enumerate(dataset):
+        color_images = norm_imgs(color_images)
+        # print(color_images)
         if i % 10 == 0:
-            l_with_ab = tf.concat([color_images[:,:,:,:1], generator(color_images)], axis=-1)
+            l_with_ab = tf.concat([color_images[...,:1], generator(color_images)], axis=-1)
             # print(decoder_out[0,100:110,100:110,0])
             # print(decoder_out[0,100:110,100:110,1])
             # print(input[0,100:110,100:110,1])
@@ -27,9 +31,9 @@ def train_epoch(generator, discriminator, dataset, new=True):
             # # print(image[100:110,100:110,2])
             # # print(l_with_ab.shape)
             
-            pil_img = tf.keras.preprocessing.image.array_to_img(tfio.experimental.color.lab_to_rgb(color_images[0]).numpy())
+            pil_img = tf.keras.preprocessing.image.array_to_img(denorm_imgs(color_images[0]).numpy())
             pil_img.show()
-            pil_img = tf.keras.preprocessing.image.array_to_img(tfio.experimental.color.lab_to_rgb(l_with_ab[0]).numpy())
+            pil_img = tf.keras.preprocessing.image.array_to_img(denorm_imgs(l_with_ab[0]).numpy())
             pil_img.show()
         with tf.GradientTape(persistent=True) as tape:
             fake_images = generator(color_images)
@@ -39,14 +43,14 @@ def train_epoch(generator, discriminator, dataset, new=True):
 
            
             d_fake = discriminator(color_images[:,:,:,:1], fake_images)
-            print(fake_images.shape)
-            print(color_images[:,:,:,1:].shape)
+            # print(fake_images.shape)
+            # print(color_images[:,:,:,1:].shape)
 
             gen_loss = generator.loss_function(fake_images, d_fake, color_images[:,:,:,1:])
 
             print(gen_loss)
 
-            d_real = discriminator(bw_images, color_images[:,:,:,1:])
+            d_real = discriminator(color_images[...,:1], color_images[:,:,:,1:])
 
             d_loss = discriminator.loss_function(d_real, d_fake)
 